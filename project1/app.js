73ca0ade82d9c075be2cc9af0559ef42f94f5979
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-01-10 17:40:57 -0500

    fixed problem

--- project1/app.js
+++ project1/app.js
@@ -1,25 +1,25 @@
-const closedFace = document.querySelector('.closed')
-const openFace = document.querySelector('.open')
-const neutralFace = document.querySelector('.neutral')
+const closedFace = document.querySelector(".closed");
+const openFace = document.querySelector(".open");
+const neutralFace = document.querySelector(".neutral");
 
 // Add event listener
-closedFace.addEventListener('click', () => {
-    if(openFace.classList.contains('open')) {
-        openFace.classList.add('active');
-        closedFace.classList.remove('active');
-    }
+closedFace.addEventListener("click", () => {
+  if (openFace.classList.contains("open")) {
+    openFace.classList.add("active");
+    closedFace.classList.remove("active");
+  }
 });
 
-openFace.addEventListener('click', () => {
-    if(neutralFace.classList.contains('neutral')) {
-        neutralFace.classList.add('active');
-        openFace.classList.remove('active');
-    }
+openFace.addEventListener("click", () => {
+  if (neutralFace.classList.contains("neutral")) {
+    neutralFace.classList.add("active");
+    openFace.classList.remove("active");
+  }
 });
 
-neutralFace.addEventListener('click', () => {
-    if(closedFace.classList.contains('closed')) {
-        closedFace.classList.add('active');
-        neutralFace.classList.remover('active');
-    }
+neutralFace.addEventListener("click", () => {
+  if (closedFace.classList.contains("closed")) {
+    closedFace.classList.add("active");
+    neutralFace.classList.remove("active");
+  }
 });
